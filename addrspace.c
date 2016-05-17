6ce0e5b8b85b4da79262fef16a7e162aa5e1dfe0
elimarhenrique <elimar.henrique@yahoo.com.br>
2016-05-17 19:33:35 -0300

    first commit

---
+++ addrspace.c
@@ -0,0 +1,27 @@
+/*
+* addrspace.c: demonstração de address space
+*
+* Para compilar:
+* cc -Wall addrspace.c -o addrspace
+*
+* Alexandre Fiori
+*/
+/* variável global */
+int varglobal;
+
+void func1(void) {
+/* variáveis de func1() */
+int var1, var2;
+}
+
+void func2(void) {
+/* variáveis de func2() */
+char str1, str2;
+}
+
+int main() {
+   /* variável de main() */
+   float calc;   
+   return 0;
+}
+
